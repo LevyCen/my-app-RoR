6b7ba36ce4d946d541bc83370102f21a6dc18c71
Levy Cen <levycen.d@gmail.com>
2021-06-29 20:20:00 -0500

    scaffolds for books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_133132) do
+ActiveRecord::Schema.define(version: 2021_06_30_005641) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.string "isbn"
+    t.integer "pages_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
